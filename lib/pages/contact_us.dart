c27775e240d5d6c64aa76059edb9e27f593edabd
Kim Quibral <kimquibral2000@gmail.com>
2023-01-22 17:52:07 +0800

    contact us

--- lib/pages/contact_us.dart
+++ lib/pages/contact_us.dart
@@ -1,7 +1,10 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:quibrals_furniture/widgets/colors.dart';
+import 'package:quibrals_furniture/widgets/dialogs.dart';
 import 'package:quibrals_furniture/widgets/texts.dart';
 import 'package:responsive_grid/responsive_grid.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUs extends StatefulWidget {
   const ContactUs({super.key});
@@ -22,6 +25,8 @@ class _ContactUsState extends State<ContactUs> {
             ResponsiveGridCol(
               sm: 6,
               child: _contactCard(
+                context,
+                facebookPageLink,
                 'assets/images/facebook-logo.png',
                 'Facebook Page',
                 facebookPageLink,
@@ -30,6 +35,8 @@ class _ContactUsState extends State<ContactUs> {
             ResponsiveGridCol(
               sm: 6,
               child: _contactCard(
+                context,
+                messengerLink,
                 'assets/images/messenger-logo.png',
                 'Messenger',
                 messengerLink,
@@ -38,6 +45,8 @@ class _ContactUsState extends State<ContactUs> {
             ResponsiveGridCol(
               sm: 6,
               child: _contactCard(
+                context,
+                gmailLink,
                 'assets/images/gmail-logo.png',
                 'Gmail',
                 gmailLink,
@@ -46,6 +55,8 @@ class _ContactUsState extends State<ContactUs> {
             ResponsiveGridCol(
               sm: 6,
               child: _contactCard(
+                context,
+                contactNumber,
                 'assets/images/mobile-call.png',
                 'Contact Number',
                 contactNumber,
@@ -57,20 +68,68 @@ class _ContactUsState extends State<ContactUs> {
     );
   }
 
-  Widget _contactCard(String imageAsset, String title, String subtitle) {
+  Widget _contactCard(
+    BuildContext context,
+    String link,
+    String imageAsset,
+    String title,
+    String subtitle,
+  ) {
     return Card(
       elevation: 10,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: transparent,
-            child: SizedBox(child: Image.asset(imageAsset)),
+      child: InkWell(
+        onTap: () => openLink(context, link),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: transparent,
+              child: SizedBox(child: Image.asset(imageAsset)),
+            ),
+            title: poppins(title, hickory, 20, fsNormal, fwBold, taLeft),
+            subtitle: poppins(subtitle, gray, 12, fsItalic, fwBold, taLeft),
           ),
-          title: poppins(title, hickory, 20, fsNormal, fwBold, taLeft),
-          subtitle: poppins(subtitle, gray, 12, fsItalic, fwBold, taLeft),
         ),
       ),
     );
+  }
+
+  Future<void> openLink(BuildContext context, String link) async {
+    if (link == gmailLink) {
+      FlutterClipboard.copy(contactNumber).then(
+        (value) => showDialog(
+          context: context,
+          builder: (context) => closeOnlyDialog(
+            context,
+            'GMAIL',
+            'Email address copied!\n$gmailLink',
+          ),
+        ),
+      );
+      closeOnlyDialog(context, 'CONTACT NUMBER', 'Contact Number copied!');
+    } else if (link == contactNumber) {
+      FlutterClipboard.copy(contactNumber).then(
+        (value) => showDialog(
+          context: context,
+          builder: (context) => closeOnlyDialog(
+            context,
+            'CONTACT NUMBER',
+            'Contact Number copied!\n$contactNumber',
+          ),
+        ),
+      );
+      closeOnlyDialog(context, 'CONTACT NUMBER', 'Contact Number copied!');
+    } else {
+      if (!await launchUrl(
+        Uri.parse(link),
+        mode: LaunchMode.externalApplication,
+      )) {
+        showDialog(
+          barrierDismissible: false,
+          context: context,
+          builder: (context) => errorDialog(context, 'Cannot open Messenger!'),
+        );
+      }
+    }
   }
 }
--- lib/widgets/dialogs.dart
+++ lib/widgets/dialogs.dart
@@ -2,39 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:quibrals_furniture/widgets/colors.dart';
 import 'package:quibrals_furniture/widgets/texts.dart';
 
-Widget checkOnlyDialog(
-  BuildContext context,
-  String title,
-  Color color,
-  double fontSize,
-  FontStyle fontStyle,
-  FontWeight fontWeight,
-  TextAlign textAlign,
-) {
-  return AlertDialog(
-    scrollable: true,
-    title: Column(
-      children: [
-        poppins(title, color, fontSize, fontStyle, fontWeight, textAlign),
-        const Divider(thickness: 2),
-      ],
-    ),
-    content: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: Icon(Icons.check, color: darkGreen),
-        ),
-      ],
-    ),
-  );
-}
-
 Widget closeOnlyDialog(
   BuildContext context,
   String title,
-  List<Widget> contents,
+  String message,
 ) {
   return AlertDialog(
     scrollable: true,
@@ -44,7 +15,7 @@ Widget closeOnlyDialog(
         const Divider(thickness: 2),
       ],
     ),
-    content: Column(children: contents),
+    content: poppins(message, black, 15, fsNormal, fwNormal, taLeft),
     actions: [
       IconButton(
         onPressed: () => Navigator.pop(context),
